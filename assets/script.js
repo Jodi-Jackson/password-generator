26934e22322c501e0a1c827d0e205bc4d74cdd71
Jodi Jackson <jjacksonjayhawkfan@gmail.com>
2021-07-25 14:31:30 -0500

    create character sets

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
-
+//event listener prompt question when button clicked
+document.querySelector("#generate").addEventListener("click", writePassword);
+// Arrays which are made up of numbers, symbols, uppercase and lowercase
+var charNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var charSym = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_","-","+","{","}","[","]",";",";","'","<",">","?","/"];
+var charLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
